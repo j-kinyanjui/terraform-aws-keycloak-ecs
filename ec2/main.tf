77d251e8e5d7587ca59fcec244512ce1d2591da6
jkinyanjui <joseph.kinyanjui@codecentric.de>
2023-02-22 19:57:56 +0100

    move from temeplate_file

--- ec2/main.tf
+++ ec2/main.tf
@@ -22,19 +22,6 @@ resource "aws_autoscaling_group" "app" {
   launch_configuration = aws_launch_configuration.app.name
 }
 
-data "template_file" "cloud_config" {
-  // add the ec2 instance to the created cluster and not the default cluster
-  template = file("ec2/templates/ecs-userdata.tpl")
-
-  vars = {
-    aws_region         = var.aws_region
-    ecs_cluster_name   = var.ecs_cluster_name
-    ecs_log_level      = var.ecs_log_level
-    ecs_agent_version  = "latest"
-    ecs_log_group_name = var.ecs_log_group_name
-  }
-}
-
 resource "aws_launch_configuration" "app" {
   security_groups = [var.instance_sg_id]
 
@@ -42,8 +29,15 @@ resource "aws_launch_configuration" "app" {
   image_id                    = data.aws_ami.aws-ecs-optimized.id
   instance_type               = var.instance_type
   iam_instance_profile        = var.app_iam_instance_profile_name
-  user_data                   = data.template_file.cloud_config.rendered
   associate_public_ip_address = true
+  user_data                   = templatefile(
+    "ec2/templates/ecs-userdata.tpl", {
+    aws_region         = var.aws_region
+    ecs_cluster_name   = var.ecs_cluster_name
+    ecs_log_level      = var.ecs_log_level
+    ecs_agent_version  = "latest"
+    ecs_log_group_name = var.ecs_log_group_name
+  })
 
   lifecycle {
     create_before_destroy = true
